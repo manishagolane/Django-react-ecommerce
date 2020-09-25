acb9f363862fcb6287eca65104d6d68a916de0ea
manishagolane <53967697+manishagolane@users.noreply.github.com>
2020-09-25 08:51:37 +0530

    Add files via upload

---
+++ ecom-frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import Routes from "./Routes";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+ReactDOM.render(<Routes/>, document.getElementById("root"));
+  
